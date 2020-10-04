5fedb51a56af5ff024c5461caaf10a515b985366
compustar <compustar@users.noreply.github.com>
2020-10-05 01:13:57 +0800

    Added tweetall.py

---
+++ tweetall.py
@@ -0,0 +1,24 @@
+from twitter import Twitter
+from ff import Browser
+import argparse
+import re
+import time
+
+parser = argparse.ArgumentParser(description='Retweet all tweets in a file')
+parser.add_argument('tweet_list', nargs='?', default='tweet_list.txt')
+parser.add_argument('--headless', action='store_true', default=False)
+parser.add_argument('--executable_path', default=r"ff\App\Firefox64\firefox.exe")
+parser.add_argument('--profile_path', default=r"ff\Data\profile")
+
+args = parser.parse_args()
+
+regex = re.compile(r"https://(?:www\.)?twitter\.com[^\s]+")
+with open(args.tweet_list, encoding="utf-8") as f:
+    with Browser() as browser:
+        twitter = Twitter(browser)
+        for line in f:
+            for url in regex.findall(line):
+                browser.driver.get(url)
+                tweet = next(twitter.get_tweets())
+                twitter.like_and_retweet(tweet)
+                time.sleep(1)
